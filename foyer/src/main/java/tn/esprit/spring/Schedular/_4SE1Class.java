4d7805e48894b3915f47f31a3c12c84805c2298a
nada-slama2 <ns1541029@gmail.com>
2025-07-06 19:10:26 +0100

    fixing pom.xmls

--- foyer/src/main/java/tn/esprit/spring/Schedular/_4SE1Class.java
+++ foyer/src/main/java/tn/esprit/spring/Schedular/_4SE1Class.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 
 @Component
 @Slf4j
@@ -11,11 +13,11 @@ public class _4SE1Class {
 
     @Scheduled(fixedDelay = 3000)
     public void fixedDelayMethod() {
-        //log.info("Hello fixedDelay "+ LocalDateTime.now());
+        log.info("Hello fixedDelay "+ LocalDateTime.now());
     }
 
     @Scheduled(fixedRate = 3000)
     public void fixedRateMethod() {
-       //log.error("Hello fixedRate "+ LocalDateTime.now());
+       log.error("Hello fixedRate "+ LocalDateTime.now());
     }
 }
